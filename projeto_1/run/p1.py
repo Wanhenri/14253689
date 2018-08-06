04766d7ec89a6d0dd8ff4743bdf2565c56b06649
jk-active <slipkorn.jk@gmail.com>
2018-08-06 17:06:33 -0300

    Update p1.py
    linhas deletadas

--- projeto_1/run/p1.py
+++ projeto_1/run/p1.py
@@ -3,14 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 from matplotlib import pyplot as plt
 
-
-time = [0, 1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 style.use('ggplot')
 
 df=pd.read_csv("48_SMG.csv",index_col='TIME', parse_dates=True)
-
-
-df1=pd.read_csv("48_SMG_wc.csv",index_col='TIME',parse_dates=True)
+#df1=pd.read_csv("48_SMG_wc.csv",index_col='TIME',parse_dates=True)
 
 
 df.plot(y="PREC") # Plot population against the index (years)
